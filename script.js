3ac67c53e4b42fa8756cc937d745b976cb17e458
Sohammmmmmmm <sohamkelkar30@gmail.com>
2025-04-16 17:11:39 +0530

    Update

--- script.js
+++ script.js
@@ -115,6 +115,11 @@ async function main() {
         document.querySelector(".left").style.left = "0"
     })
 
+     // Add an event listener for close button
+
+     document.querySelector(".close").addEventListener("click",()=>{
+        document.querySelector(".left").style.left = "-120%"
+    })
 
    
 
